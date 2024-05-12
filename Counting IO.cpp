bc796198b496d43633e5c80e9b0edea10c236282
Martin Mathurine <42855193+martinmathurine@users.noreply.github.com>
2024-05-12 21:52:18 +0100

    Variables and Simple IO

---
+++ Counting IO.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+
+{
+	int count1 = 10;
+	int count3 = 50;
+	cout << "Value of outer count1 = " << count1 << endl;
+
+	{
+		int count1 = 20;
+		int count2 = 30;
+		cout << "Value of inner count1 = " << count1 << endl;
+		count1 += 3;
+		count3 += count2;
+	}
+
+	cout << "Value of outer count1 = " << count1 << endl;
+	cout << "Value of outer count3 = " << count3 << endl;
+	cout << count3 << endl;
+
+	return 0;
+}
